3b27ca8f602fa3a0dacd53990ff943bb138b5fb8
Alex Zaytsev <hazzik@gmail.com>
2021-03-05 17:10:35 +1300

    Add CH-UserID and CH-DeviceId header names

--- ClubhouseDotNet/HeaderNames.cs
+++ ClubhouseDotNet/HeaderNames.cs
@@ -6,5 +6,7 @@
         public const string ChLocale = "CH-Locale";
         public const string ChAppBuild = "CH-AppBuild";
         public const string ChAppVersion = "CH-AppVersion";
+        public const string ChUserId = "CH-UserID";
+        public const string CHDeviceId = "CH-DeviceId";
     }
 }
